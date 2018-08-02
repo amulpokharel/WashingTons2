6e9e139acbb16553e1ccc5bf0fca05781f220405
Amul Pokharel <amulpokh@gmail.com>
2018-08-02 13:37:09 -0400

    methods to viewmodel to get the list for init'ing the recyclerviews

--- app/src/main/java/amulp/com/washingtons2/ui/main/MainViewModel.kt
+++ app/src/main/java/amulp/com/washingtons2/ui/main/MainViewModel.kt
@@ -1,7 +1,10 @@
 package amulp.com.washingtons2.ui.main
 
+import amulp.com.washingtons2.utils.ListProvider
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-
+    fun getWashList() = ListProvider.getWasherList()
+    fun getDryList() = ListProvider.getDryerList()
+    fun getOtherList() = ListProvider.getOtherList()
 }
